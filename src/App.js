ab9294b3a8cad4e1ee0b0e1eab640f73a369e57f
giabach99 <giabach060399@gmail.com>
2023-03-30 16:57:53 +0700

    deploy again after testing

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends React.Component {
 
     onButtonSubmit = () => {               
         this.setState({imageUrl: this.state.input})
-        fetch('http://localhost:3000/imageUrl', {
+        fetch('https://render-backend-bg94.onrender.com/imageUrl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -74,7 +74,7 @@ class App extends React.Component {
         .then(response => response.json())
         .then(response => {
             if (response) {
-                fetch('http://localhost:3000/image', {
+                fetch('https://render-backend-bg94.onrender.com/image', {
                     method: 'put',
                     headers: {
                         'Content-Type': 'application/json'
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://render-backend-bg94.onrender.com/signin', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
