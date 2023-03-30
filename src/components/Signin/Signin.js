ba2fbcfd39a3fe77de7dd5cafda864bd38cab0a6
giabach99 <giabach060399@gmail.com>
2023-03-30 17:48:45 +0700

    change fetch url

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://render-backend-bg94.onrender.com/signin', {
+        fetch('https://my-backend-qshg.onrender.com/signin', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
